84944bf89fd85fdd132ec1d9021ad2e2ca1d6804
Sudheer-babu-meduri <sudheerchowdary156@gmail.com>
2025-11-25 12:37:18 +0530

    Update BookTickets.java

--- CoreJavaSudheer/src/com/multiThreading/BookTickets.java
+++ CoreJavaSudheer/src/com/multiThreading/BookTickets.java
@@ -3,8 +3,9 @@ package com.multiThreading;
 class BookMyShow{
 	int total_Tickets=100;
 	
-	synchronized void bookseats(int seats) {
-		if(seats<=total_Tickets) {
+	synchronized void bookseats(int seats) {                 // Shared Resource , everyone is coming here to book tickets 
+		if(seats<=total_Tickets) {                           // it is giving tickets in synchronized way it means one after another
+                                                             //without synchronization it may give will give data inconsistency
 			System.out.println("Your "+seats+" Tickets Has Been Booked ");
 			total_Tickets = total_Tickets-seats;
 			System.out.println("Remaining Available Tickets are "+total_Tickets);
@@ -17,7 +18,7 @@ class BookMyShow{
 
 
 public class BookTickets extends Thread{
-	static BookMyShow b;
+	static BookMyShow b;             
 	int seats;
 	@Override
 	public void run() {
@@ -26,10 +27,10 @@ public class BookTickets extends Thread{
 	  }
 
 	public static void main(String[] args) {
-        b=new BookMyShow();
+        b=new BookMyShow();                  // Object for Shared resource BookMyShow  which is static(shared)
 		
-		BookTickets s1 =new BookTickets();
-		BookTickets s2 =new BookTickets();
+		BookTickets s1 =new BookTickets();         // N no.of objects(peoples) uses the shared resource 
+		BookTickets s2 =new BookTickets();          
 		BookTickets s3 =new BookTickets();
 		BookTickets s4 =new BookTickets();
 		BookTickets s5 =new BookTickets();
@@ -39,12 +40,13 @@ public class BookTickets extends Thread{
 
 		s1.seats=8;
 		s2.seats=12;
-		s3.seats=14;
-		s4.seats=10;
+		s3.seats=14;         //every person wants their requried seats and every one should get fair tickets 
+	    s4.seats=10;        // or get proper message if tickets are unavailable
 		s5.seats=15;
 		s6.seats=25;
 		s7.seats=35;
-		s8.seats=18;
+		s8.seats=18;                      
+		 
 		
 		s1.start();
 		s2.start();
@@ -58,3 +60,4 @@ public class BookTickets extends Thread{
 	 }
 
 }
+
